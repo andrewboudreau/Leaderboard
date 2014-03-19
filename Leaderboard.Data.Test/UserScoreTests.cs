e95b23daadd6fbffc84cde3807241304f9f58dff
Andrew <andrewboudreau@gmail.com>
2014-03-19 18:35:07 -0500

    Test and package up

--- Leaderboard.Data.Test/UserScoreTests.cs
+++ Leaderboard.Data.Test/UserScoreTests.cs
@@ -32,10 +32,55 @@ namespace Leaderboard.Data.Test
             using (var context = new LeaderboardContext())
             {
                 var user = context.Users.Single(u => u.Id == userId);
-                
+
                 Assert.AreEqual(userId, user.Id);
                 Assert.AreEqual(1, user.Scores.Count(), "User should have 1 score");
                 Assert.AreEqual(10, user.Scores.Single().Score, "User should have a score of 10");
+            }
+        }
+
+        [TestMethod]
+        public void CreateUserAddMultipleScores()
+        {
+            int userId;
+            using (var context = new LeaderboardContext())
+            {
+                var user = context.Users.Create();
+                context.Users.Add(user);
+                user.Name = Guid.NewGuid().ToString();
+
+                context.SaveChanges();
+
+                userId = user.Id;
+            }
+
+            using (var context = new LeaderboardContext())
+            {
+                var user = context.Users.Single(u => u.Id == userId);
+
+                for (var i = 0; i < 10; i++)
+                {
+                    var score = context.UserScores.Create();
+                    context.UserScores.Add(score);
+                    score.Score = i;
+                    score.User = user;
+                    score.Created = DateTime.UtcNow;
+                }
+
+                context.SaveChanges();
+            }
+
+            using (var context = new LeaderboardContext())
+            {
+                var user = context.Users.Single(u => u.Id == userId);
+
+                Assert.AreEqual(userId, user.Id);
+                Assert.AreEqual(10, user.Scores.Count(), "User should have 10 score");
+
+                for (var i = 0; i < 10; i++)
+                {
+                    Assert.AreEqual(i, user.Scores.ElementAt(i).Score, "User score should match index");
+                }
             }
         }
     }
---
+++ Leaderboard.Web/Controllers/Default1Controller.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Leaderboard.Data;
+
+namespace Leaderboard.Web.Controllers
+{
+    public class Default1Controller : ApiController
+    {
+        private LeaderboardContext db = new LeaderboardContext();
+
+        // GET api/Default1
+        public IQueryable<UserScore> GetUserScores()
+        {
+            return db.UserScores;
+        }
+
+        // GET api/Default1/5
+        [ResponseType(typeof(UserScore))]
+        public IHttpActionResult GetUserScore(int id)
+        {
+            UserScore userscore = db.UserScores.Find(id);
+            if (userscore == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userscore);
+        }
+
+        // PUT api/Default1/5
+        public IHttpActionResult PutUserScore(int id, UserScore userscore)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userscore.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(userscore).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserScoreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Default1
+        [ResponseType(typeof(UserScore))]
+        public IHttpActionResult PostUserScore(UserScore userscore)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.UserScores.Add(userscore);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = userscore.Id }, userscore);
+        }
+
+        // DELETE api/Default1/5
+        [ResponseType(typeof(UserScore))]
+        public IHttpActionResult DeleteUserScore(int id)
+        {
+            UserScore userscore = db.UserScores.Find(id);
+            if (userscore == null)
+            {
+                return NotFound();
+            }
+
+            db.UserScores.Remove(userscore);
+            db.SaveChanges();
+
+            return Ok(userscore);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserScoreExists(int id)
+        {
+            return db.UserScores.Count(e => e.Id == id) > 0;
+        }
+    }
+}
