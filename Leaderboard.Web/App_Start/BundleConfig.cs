2947cf504a7acf016537e21d2459439f02a3bc27
andrewboudreau <andrewboudreau@gmail.com>
2014-03-23 05:43:46 -0500

    disabled minification
    http://stackoverflow.com/questions/11944745/asp-net-bundles-how-to-disable-minification

--- Leaderboard.Web/App_Start/BundleConfig.cs
+++ Leaderboard.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace Leaderboard.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // http://stackoverflow.com/questions/11944745/asp-net-bundles-how-to-disable-minification
+            System.Web.Optimization.BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                        "~/Scripts/jquery-{version}.js",
                        "~/Scripts/ajaxPrefilters.js",
