b889d6b7bd15ba7e6c552eb74aabd9fdff7e6c7d
andrewboudreau <andrewboudreau@gmail.com>
2014-03-23 11:42:46 -0500

    was old commit

--- Leaderboard.Web/App_Start/Startup.Auth.cs
+++ Leaderboard.Web/App_Start/Startup.Auth.cs
@@ -40,6 +40,11 @@ namespace Leaderboard.SecureWeb
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
             // Enable the application to use bearer tokens to authenticate users
             app.UseOAuthBearerTokens(OAuthOptions);
             
