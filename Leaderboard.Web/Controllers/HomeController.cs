d7e1d60abeba942bb8abf6a5d9a2983c4a5cb912
andrewboudreau <andrewboudreau@gmail.com>
2014-03-23 20:40:35 -0500

    ui tweaks

--- Leaderboard.Web/Controllers/HomeController.cs
+++ Leaderboard.Web/Controllers/HomeController.cs
@@ -20,7 +20,13 @@ namespace Leaderboard.Web.Controllers
         public ActionResult Leaderboard()
         {
             ViewBag.Title = "Leaderboard Screen";
-            var scores = this.db.UserScores.ToLookup(k => k.UserName);
+            var scores = this.db.UserScores
+                .GroupBy(m => m.UserName)
+                .ToList()
+                .Select(l => new Tuple<string, int>(l.Key, l.Max(x => x.Score)))
+                .OrderByDescending(s => s.Item2)
+                .ToList();
+
             return View(scores);
         }
 
